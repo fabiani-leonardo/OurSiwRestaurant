7d0a88997aebfeed1de428ac1870ad63af6467ca
ChiaraBranca <lisabranca11@gmail.com>
2025-05-15 09:56:26 +0200

    creazione classe ristorante

---
+++ src/main/java/it/uniroma3/siw/model/Ristorante.java
@@ -0,0 +1,15 @@
+package it.uniroma3.siw.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Ristorante {
+	
+	public String nome;
+	public String indirizzo;
+	public Integer numero;
+	public String immagine;
+	
+	
+
+}
